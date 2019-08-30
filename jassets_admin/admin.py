53e33f1c4b984c3303b31142c3354044c7257b1c
konstantinprilepin <konstantin.prilepin@jibrel.network>
2019-08-30 18:34:57 +0300

    added some config to admin classes

--- jassets_admin/admin.py
+++ jassets_admin/admin.py
@@ -3,23 +3,35 @@ from django.contrib import admin
 from .models import TradingPair, Platform, Asset, Exchange
 
 
-class PlatformAdmin(admin.ModelAdmin):
+class BaseModelAdmin(admin.ModelAdmin):
+    list_per_page = 25
+
+
+class PlatformAdmin(BaseModelAdmin):
     list_display = ('id', 'name', 'symbol', 'main_asset_obj')
+    list_select_related = ('main_asset_obj', )
+    search_fields = ('name', 'symbol')
+    list_filter = ('main_asset_obj__type', 'main_asset_obj__is_active')
 
 
-class AssetAdmin(admin.ModelAdmin):
-    list_display = ('id', 'uuid', 'name', 'description', 'platform_obj', 'symbol',
-                    'type', 'is_active', 'address', 'properties', 'created',
-                    'updated')
+class AssetAdmin(BaseModelAdmin):
+    list_display = ('id', 'uuid', 'name', 'description', 'platform_obj', 
+                    'symbol', 'type', 'is_active', 'address', 'properties', 
+                    'created', 'updated')
+    list_select_related = ('platform_obj', )
+    search_fields = ('name', 'symbol')
+    list_filter = ('platform_obj', 'type', 'is_active', 'created', 'updated')
 
 
-class ExchangeAdmin(admin.ModelAdmin):
+class ExchangeAdmin(BaseModelAdmin):
     list_display = ('id', 'name', 'slug', 'url')
+    search_fields = ('name', 'slug')
 
 
-class TradingPairAdmin(admin.ModelAdmin):
+class TradingPairAdmin(BaseModelAdmin):
     list_display = ('id', 'base_asset_obj', 'quote_asset_obj', 'exchange_obj',
                     'symbol')
+    list_select_related = ('base_asset_obj', 'quote_asset_obj', 'exchange_obj')
 
 
 admin.site.register(Platform, PlatformAdmin)
